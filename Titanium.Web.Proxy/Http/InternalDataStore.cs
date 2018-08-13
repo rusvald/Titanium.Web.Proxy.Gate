e2f87148bfc1df6c9b9ef632c748f6130f5bac89
Ruslan <rusvald80@gmail.com>
2018-08-13 09:13:32 +0300

    Init Project from source

---
+++ Titanium.Web.Proxy/Http/InternalDataStore.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+
+namespace Titanium.Web.Proxy.Http
+{
+    class InternalDataStore : Dictionary<string, object>
+    {
+        public bool TryGetValueAs<T>(string key, out T value)
+        {
+            object value1;
+            bool result = TryGetValue(key, out value1);
+            if (result)
+            {
+                value = (T)value1;
+            }
+            else
+            {
+                value = default(T);
+            }
+
+            return result;
+        }
+
+        public T GetAs<T>(string key)
+        {
+            return (T)this[key];
+        }
+    }
+}
